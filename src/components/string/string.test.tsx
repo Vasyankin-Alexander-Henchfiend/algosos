5559376d5e707b88fe34c68a1c0fdc786b6c850c
Александр Васянькин <henchfiend@yandex.ru>
2024-01-31 11:07:26 +0300

    Unit-тесты ч.2

--- src/components/string/string.test.tsx
+++ src/components/string/string.test.tsx
@@ -1,18 +1,47 @@
-import renderer from "react-test-renderer";
 import { sortArray } from "./utils";
 import { TArrayElement } from "./string.types";
 import { ElementStates } from "../../types/element-states";
 
-async function dummy(x: Array<TArrayElement>) {}
+async function emptyRefreshFunc(someArray: Array<TArrayElement>) {}
 
 describe("Тестирование алгоритма разворота строки", () => {
-  test("результат с чётным количеством символов", async () => {
+  test("Результат с чётным количеством символов", async () => {
     const testArray = ["1", "2", "3", "4"].map((item) => {
       return { state: ElementStates.Default, value: item } as TArrayElement;
     });
-    const result = (await sortArray(testArray, dummy)).map(
+    const result = (await sortArray(testArray, emptyRefreshFunc)).map(
       (item) => item.value
     );
     expect(result).toStrictEqual(["4", "3", "2", "1"]);
+  });
+
+  test("Результат с нечетным количеством символов", async () => {
+    const testArray = ["1", "2", "3", "4", "5"].map((item) => {
+      return { state: ElementStates.Default, value: item } as TArrayElement;
+    });
+    const result = (await sortArray(testArray, emptyRefreshFunc)).map(
+      (item) => item.value
+    );
+    expect(result).toStrictEqual(["5", "4", "3", "2", "1"]);
+  });
+
+  test("Результат с одним символом", async () => {
+    const testArray = ["1"].map((item) => {
+      return { state: ElementStates.Default, value: item } as TArrayElement;
+    });
+    const result = (await sortArray(testArray, emptyRefreshFunc)).map(
+      (item) => item.value
+    );
+    expect(result).toStrictEqual(["1"]);
+  });
+
+  test("Результат с пустой строкой", async () => {
+    const testArray = [""].map((item) => {
+      return { state: ElementStates.Default, value: item } as TArrayElement;
+    });
+    const result = (await sortArray(testArray, emptyRefreshFunc)).map(
+      (item) => item.value
+    );
+    expect(result).toStrictEqual([""]);
   });
 });
